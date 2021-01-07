b62e6e8de968af4776a60f13a6970652376d38a8
EC2 Default User <ec2-user@ip-172-31-38-217.ap-northeast-1.compute.internal>
2021-01-07 16:44:54 +0000

    first-commit

---
+++ lesson6.js
@@ -0,0 +1,9 @@
+$(function() {
+	$('.animals-slick-box-list').slick({
+		  autoplay: true, 
+		  autoplaySpeed: 4000,
+	    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    　nextArrow: '<button class="slide-arrow next-arrow"></button>'
+	}); 
+});
+
